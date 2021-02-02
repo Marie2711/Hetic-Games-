47ff7073241035ad9a92d9d29743e2b21c0e46fb
Aouis20 <aouis.choukri@hetic.net>
2021-02-02 21:36:03 +0100

    MemoryGameSave

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-function Jeupaire() {
-    console.log("je suis le log")
-}
+function swapimage() {
+    console.log("getimage")
+    let elem = document.getElementById("carte1")
+    elem.innerHTML += "<span>mdrrrr</span>"
+    console.log(elem)
+  }
