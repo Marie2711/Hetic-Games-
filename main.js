e0adff51e525aac61f1737bb788a301fa4a8794b
Aouis20 <aouis.choukri@hetic.net>
2021-02-03 12:53:35 +0100

    cardswap

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 function swapimage() {
-    console.log("getimage")
-    let elem = document.getElementById("carte1")
-    elem.innerHTML += "<span>mdrrrr</span>"
+  let classe = document.querySelector("img").classList.contains("off")
+  console.log(classe)
+  let elem = document.querySelector("#carte1")
+  if (classe) {
+    console.log("c'est le else")
+    elem.classList.replace("off","on")
+    elem.src='img/mouton.png'
     console.log(elem)
   }
+  else {
+    console.log("c'est le else")
+    elem.classList.replace("on","off")
+    elem.src='img/cartefacecache.png'
+    console.log(elem)
+    // marche mes retourne la carte instant car éxécution du code => instant
+    // transition à mettre jusqu'à 'click' d'une autre carte
+  }
+}
