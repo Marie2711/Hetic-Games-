181b82a1355150bdb1313b987fbde03ec5db3f94
Aouis20 <aouis.choukri@hetic.net>
2021-02-04 11:03:43 +0100

    Show and unshowed

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-function swapimage() {
-  let classe = document.querySelector("img").classList.contains("off")
-  console.log(classe)
-  let elem = document.querySelector("#carte1")
+function swapimage(id,animal) {
+  // identification de la carte
+  let monid = "#" + id
+  // identification de l'animal sous la carte
+  let card = 'img/' + animal + '.png'
+  // Vérification de la classe (on ou off)
+  let classe = document.querySelector(monid).classList.contains("off")
+  console.log("Faut-il retourné la carte ?",classe)
+
+  // Modification de la carte
+  let elem = document.querySelector(monid)
+    // Si off (montrer l'animal)
   if (classe) {
-    console.log("c'est le else")
     elem.classList.replace("off","on")
-    elem.src='img/mouton.png'
+    elem.src=card
     console.log(elem)
   }
+    // sinon (retourner la carte face caché)
   else {
-    console.log("c'est le else")
     elem.classList.replace("on","off")
     elem.src='img/cartefacecache.png'
     console.log(elem)
-    // marche mes retourne la carte instant car éxécution du code => instant
-    // transition à mettre jusqu'à 'click' d'une autre carte
   }
+  // COMPARAISON AVEC LA SECONDE CARTE
 }
