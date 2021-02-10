4d23745aab770e2043ed3286b76a5a7819be9d7c
Noham Hirep <noham.hirep@hetic.net>
2021-02-10 17:13:27 +0100

    TallyClickCounter Js

--- TallyClickCounter/main.js
+++ TallyClickCounter/main.js
@@ -11,6 +11,10 @@ function onClick() {
 click += 1;
 document.getElementById("click").innerHTML = click;
 };
+function reset() {
+click = 0 ;
+document.getElementById("click").innerHTML = click;
+};
 
 
 
