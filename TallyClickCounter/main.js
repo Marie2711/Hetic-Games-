cdfa969f6def01d9eaa7caa5b2d6b69cec5e2cbc
Marie2711 <74191815+Marie2711@users.noreply.github.com>
2021-02-10 15:35:41 +0100

    Add files via upload

---
+++ TallyClickCounter/main.js
@@ -0,0 +1,18 @@
+
+/*
+    -------------------------------
+         Author: Hirep Noham
+    -------------------------------
+*/
+
+
+var click = 0;
+function onClick() {
+click += 1;
+document.getElementById("click").innerHTML = click;
+};
+
+
+
+
+
