fd8d1d7f85ac27dc599f303d9bd8b7861bd02c26
Noham Hirep <noham.hirep@hetic.net>
2021-02-12 10:52:40 +0100

    Hetic Game

---
+++ Game/TallyClickCounter/main.js
@@ -0,0 +1,22 @@
+
+/*
+    -------------------------------
+         Author: Hirep Noham
+    -------------------------------
+*/
+
+
+var click = 0;
+function onClick() {
+click += 1;
+document.getElementById("click").innerHTML = click;
+};
+function reset() {
+click = 0 ;
+document.getElementById("click").innerHTML = click;
+};
+
+
+
+
+
