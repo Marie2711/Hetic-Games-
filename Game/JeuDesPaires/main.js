4e304cc7b41040c8dd558b3dba9bf6ff043ad5a3
Aouis20 <78351892+Aouis20@users.noreply.github.com>
2021-02-12 17:12:46 +0100

    Update main.js

--- Game/JeuDesPaires/main.js
+++ Game/JeuDesPaires/main.js
@@ -19,7 +19,6 @@ function go() {
     else {
       timer = 0
       document.getElementById("timer").innerHTML = timer
-      console.log("timer :",timer)
       document.querySelector("#memorygame").id="lose"
       document.querySelector("#lose").innerHTML="<h2>Manque de temps tu as perdu...</h2>"
       document.querySelector("#lose").innerHTML+="<a id='go' href='index.html'>Recommencer</a>"
@@ -58,7 +57,6 @@ function generate() {
   allanimals.reverse()
   myanimals = allanimals
   allanimals = null
-  console.log(myanimals)
   for (let i = (myanimals.length-1); i >= 0; i--) {
     document.getElementById("carte"+[i]).src = "img/thecartefacecache.png"
   }
@@ -78,11 +76,8 @@ function compar(id,animal) {
       mesid[1] = id
       myanimal[1] = animal
     }
-    else {
-      console.log("ne rien faire ")
-    }
+    else {}
     if (mesid[0] && mesid[1] && myanimal[0] && myanimal[1]) {
-      console.log(mesid, myanimal)
       swapimage()
     }
   }
@@ -129,7 +124,6 @@ function swapimage() {
         document.querySelector("#score").innerHTML = score
       }
     },1000);
-    console.log("mon score:",score)
     mesid = []
     myanimal = []
   }
@@ -139,7 +133,6 @@ function openimage(card,ani) {
   card.classList.replace("off","on")
   card.src=ani
   card.style=null
-  console.log(card)
 }
 
 function win(card) {
