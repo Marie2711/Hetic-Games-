39bbb0457f58a0aeea87d43e74ac1e1e5fdfe74e
Noham Hirep <noham.hirep@hetic.net>
2021-02-12 11:03:05 +0100

    Hetic Game

---
+++ Game/JeuDesPaires/README.md
@@ -0,0 +1,37 @@
+INSTALLATION GITHUB
+
+1- Installer Github :
+    https://git-scm.com/download/win
+
+2- Crée un dossier sur votre bureau afin de le connecter au Github
+
+3- Ouvrir le terminal Git (Git Bash)
+
+4- Accéder au dossier (cd ..):
+    Si votre dossier est sur le bureau faire:
+    
+    - cd Desktop
+    
+    - cd (nom du fichier)
+
+5- Intégrer le contenu de git dans votre dossier avec la commande (mettre ce lien):
+
+    git clone https://github.com/Marie2711/projet-de-ouf-malade
+
+
+UTILISATION GITHUB
+
+1- Aller dans son dossier
+
+2- git checkout -b (nom de la branche)=> voir s'il y a des bug pour la branch
+
+3- Toujours vérifier si vous êtes à jours (pour pas écraser le travail d'un autre):
+
+    git pull (si up to date => c'est bon on peut commencer a modifié nos fichiers)
+
+4- Après les avoir modifié il faut sauvegarder son travail:
+
+    git status (affiche normalement une alerte en rouge précisant le ou les fichier(s) modifié(s))  #Afficher les modifications
+    git add (nomdufichierenrouge)	(si plusieurs fichiers répété la commande)                      #Ajouter des fichiers
+    git commit -m "commentaire..."  (dans commentaires, mettre les raisons)                         #Qu'est ce qui a été modifié
+    git push	                                                                                #Envoyez les données
---
+++ js/menu.js
@@ -0,0 +1,100 @@
+
+
+var decalage = 0;
+var nbBout;	  
+var timer;
+
+function menu(nb)
+{
+ var angle = 360 / nb;		
+ nbBout = nb;
+ var posX;
+ var posY;
+ var rayon = 60;
+ var centreX = document.getElementById("centre").offsetLeft;
+ var centreY = document.getElementById("centre").offsetTop;
+ var lItem = 20;
+ var i = 1;
+ var item;
+ var b;
+ 
+for(b = 0; b < 360; b += angle)
+ {		  
+  posX = centreX + 25 + rayon * Math.cos(b * Math.PI / 180);
+  posY = centreY + 25 + rayon * Math.sin(b * Math.PI / 180); 
+  
+  item = document.getElementById("item" + i);
+  item.className = "item";	
+  item.style.top = (posY - lItem / 2) + "px"; 
+  item.style.left = (posX - lItem / 2) + "px";
+  i++;
+ }
+}
+
+function anim()
+{		  
+ decalage++;
+ var angle = 360 / nbBout;		
+ var posX;
+ var posY;
+ var rayon = 70;
+ var centreX = document.getElementById("centre").offsetLeft;
+ var centreY = document.getElementById("centre").offsetTop;
+ var lCentre = 50;
+ var lItem = 20;
+ var i = 1;
+ var item
+ var b;
+
+ for(b = 0; b < 360; b = b + angle)
+ {		  
+  posX = centreX + 25 + rayon * Math.cos((b + decalage) * Math.PI / 180);
+  posY = centreY + 25 + rayon * Math.sin((b + decalage) * Math.PI / 180); 
+  
+  item = document.getElementById("item" + i);
+  item.style.top = (posY - lItem / 2) + "px"; 
+  item.style.left = (posX - lItem / 2) + "px";
+  i++;
+ }	
+}
+
+timer = setInterval("anim()",50);
+
+function stopanim()
+{
+ clearInterval(timer);		   
+}
+
+function replayanim()
+{
+ timer = setInterval("anim()",50);
+}
+
+document.onmousemove = suitsouris;
+function suitsouris(evenement)
+{
+        if(navigator.appName=="Microsoft Internet Explorer")
+        {
+                var x = event.x+document.body.scrollLeft;
+        }
+        else
+        {
+                var x =  evenement.pageX;
+        }
+        document.getElementById("image_suit_souris").style.left = (x)+'px';
+    }
+
+
+
+function changeImage1() {
+
+        if (document.getElementById("nathan").src == "img/nathan.svg") 
+        {
+            document.getElementById("nathan").src = "img/nathan1.svg";
+            console.log("blabla")
+        }
+        else 
+        {
+            document.getElementById("nathan").src = "img/nathan.svg";
+        }
+    }
